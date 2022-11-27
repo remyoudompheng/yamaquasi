7d015e895d5275316815cc60b011a1ed27f92114
Rémy Oudompheng <remyoudompheng@gmail.com>
2022-11-27 18:10:44 +0100

    Helper script to generate examples

---
+++ semiprimes.py
@@ -0,0 +1,25 @@
+import sys
+from random import randint
+from gmpy2 import mpz, next_prime, is_prime
+
+
+def main():
+    if len(sys.argv) != 2:
+        print(f"Usage: {sys.argv[0]} N_BITS")
+        exit(1)
+    size = int(sys.argv[1])
+    seed = mpz(randint(2 ** (size // 2 - 1), 2**(size // 2)))
+    primes = []
+    while len(primes) < 2:
+        seed = next_prime(seed)
+        if is_prime(seed // 2):
+            primes.append(int(seed))
+            seed = mpz(randint(2 ** (size // 2 - 1), 2**(size // 2)))
+    p, q = primes
+    print(f"{p =}")
+    print(f"{q =}")
+    print(f"n = {p*q}")
+
+
+if __name__ == "__main__":
+    main()
