7cfa6d25e5e487019b9c34f7884b9c3bab29fda9
Rémy Oudompheng <remyoudompheng@gmail.com>
2022-12-09 19:51:10 +0100

    benchmark script

--- benchmarks.sh
+++ benchmarks.sh
@@ -2,12 +2,28 @@
 
 MODE=${1:-mpqs}
 
-for bits in 40 60 80 100 120 140; do
-	# Prepare random inputs
-	for i in {0..50}; do
-	    python semiprimes.py $bits
-        done 1> testinputs.txt 2>/dev/null
-	args=${args%,}
-	echo "=== INPUT $bits bits ==="
-	hyperfine -m 10 "bin/ymqs --mode $MODE "'$(shuf -n 1 testinputs.txt)'
+case $MODE in
+    msieve)
+        COMMAND="msieve -l /dev/null -v"
+        ;;
+    flintqs)
+        COMMAND="QuadraticSieve <<<"
+        ;;
+    qs|mpqs|siqs)
+        COMMAND="bin/ymqs --mode $MODE"
+        ;;
+    *)
+        echo Unknown mode $MODE
+        exit 1
+        ;;
+esac
+
+for bits in 40 60 80 100 120 140 160 180 200 220; do
+    # Prepare random inputs
+    for i in {0..50}; do
+        python semiprimes.py $bits
+    done 1> testinputs.txt 2>/dev/null
+    args=${args%,}
+    echo "=== INPUT $bits bits ==="
+    hyperfine -i -m 10 -p "rm -f msieve.dat" "$COMMAND "'$(shuf -n 1 testinputs.txt)'
 done
