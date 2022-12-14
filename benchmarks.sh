f6552b97bb03e7cb45d7e4d63113d9172e6155c3
Rémy Oudompheng <remyoudompheng@gmail.com>
2022-12-14 08:59:18 +0100

    Update benchmarks

--- benchmarks.sh
+++ benchmarks.sh
@@ -15,18 +15,21 @@ case $MODE in
     mpqs6)
         COMMAND="bin/ymqs --mode mpqs --threads 6"
         ;;
+    siqs6)
+        COMMAND="bin/ymqs --mode siqs --threads 6"
+        ;;
     *)
         echo Unknown mode $MODE
         exit 1
         ;;
 esac
 
-for bits in 40 60 80 100 120 140 160 180 200 220; do
+for bits in 40 60 80 100 120 140 160 180 200 220 240 260; do
     # Prepare random inputs
     for i in {0..50}; do
         python semiprimes.py $bits
-    done 1> testinputs.txt 2>/dev/null
+    done 1> testinputs$bits.txt 2>/dev/null
     args=${args%,}
     echo "=== INPUT $bits bits ==="
-    hyperfine -i -m 10 -S "bash --norc" -p "rm -f msieve.dat" "$COMMAND "'$(shuf -n 1 testinputs.txt)'
+    hyperfine -i -m 10 -S "bash --norc" -p "rm -f msieve.dat" "$COMMAND \$(shuf -n 1 testinputs$bits.txt)"
 done
