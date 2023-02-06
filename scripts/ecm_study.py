7c3def34e8b9474206c6fc40bdcf3bb3448bce39
Rémy Oudompheng <remyoudompheng@gmail.com>
2023-02-06 22:03:03 +0100

    Include Suyama curves (twisted Edwards) in ECM efficiency study script
    Suyama curves are nearly as effective as the best EECM good curves,
    and can be represented as a=-1 twisted Edwards curves. The script
    confirms that the largest prime factor of curve order can be about
    2x smaller than in the case of the Z/2 x Z/4 curves.

--- scripts/ecm_study.py
+++ scripts/ecm_study.py
@@ -1,6 +1,31 @@
-import random
-from random import getrandbits
-from sage.all import euler_phi, factor, primes, proof, GF, EllipticCurve, QQ
+"""
+Study efficiency of ECM parameters
+
+The study of curve orders uses the
+equivalence between twisted Edwards and Weierstrass forms:
+    a x^2 + y^2 = 1 + d x^2 y^2
+is equivalent to:
+    B y^2 = x^3 + A x^2 + x
+    where A = 2(a+d)/(a-d), B=4/(a-d)
+and equivalent to
+    y^2 = x^3 + (A/B) x + (1/B^2)
+
+If c=(a-d)/4
+1/B^2 = c^2
+A/B = (a+d)/2 = a-2c
+
+References:
+
+Daniel J. Bernstein, Peter Birkner, Tanja Lange, Christiane Peters. ECM using Edwards curves
+https://eecm.cr.yp.to/eecm-20111008.pdf
+
+Razvan Barbulescu, Joppe W. Bos, Cyril Bouvier, Thorsten Kleinjung, Peter L.
+Montgomery. Finding ECM-friendly curves through a study of Galois properties.
+ANTS-X 10th Algorithmic Number Theory Symposium - 2012
+https://hal.inria.fr/hal-00671948v2
+"""
+
+from sage.all import euler_phi, factor, random_prime, proof, GF, EllipticCurve, QQ
 from math import gcd
 
 proof.arithmetic(False)
@@ -15,17 +40,54 @@ for k in range(4, 21):
             nbest = n
     phi = euler_phi(nbest) // 2
     for d2 in [2 * t, 3 * t, 4 * t]:
-        print(f"D1={nbest} D2={d2} B2={float(d2*nbest):.3e} φ(D)/2={phi} ({d2//t} blocks size 2^{k-1})")
+        print(
+            f"D1={nbest} D2={d2} B2={float(d2*nbest):.3e} φ(D)/2={phi} ({d2//t} blocks size 2^{k-1})"
+        )
 
-def analyze(primes, x, y, expect=1):
+# Moduli space of Suyama-11 curves.
+X13f = EllipticCurve(QQ, [0, -1, 0, -120, 432])
+
+
+def x13f_params(P):
+    PX, PY = P.xy()
+    sigma = 24 / PX - 1
+    alpha = sigma**2 - 5
+    beta = 4 * sigma
+    r = 48 * PY / PX**2
+    if sigma not in (0, 1, 5, -5):
+        x = (2 * r * sigma) / (sigma - 1) / (sigma + 5) / (sigma**2 + 5)
+        y = (alpha**3 - beta**3) / (alpha**3 + beta**3)
+    else:
+        x, y = "invalid", "invalid"
+    print(f"P={P} σ={sigma} r={r} G=({x}, {y})")
+    return sigma, r, x, y
+
+
+print("Suyama-11 family details:")
+x13f_t = X13f.torsion_points()
+print("Torsion points (invalid)")
+for t in x13f_t:
+    if t[0] == 0:
+        continue
+    x13f_params(t)
+
+pg = X13f.gens()[0]
+for k in (1, 2, 3):
+    print(f"{k}G + torsion")
+    for t in x13f_t:
+        x13f_params(k * pg + t)
+
+
+def analyze(primes, b1, b2, x, y, a=1, expect=1):
     stats1, stats2 = [], []
     exp2, exp3 = 0, 0
     t1, t2 = 0, 0
+    success = 0
     for p in primes:
         K = GF(p)
-        d = K(x * x + y * y - 1) / K(x * x * y * y)
-        c = (1 - d) / 4
-        E = EllipticCurve(GF(p), [0, 1 - 2 * c, 0, c * c, 0])
+        d = K(a * x * x + y * y - 1) / K(x * x * y * y)
+        c = (K(a) - d) / 4
+        E = EllipticCurve(GF(p), [0, K(a) - 2 * c, 0, c * c, 0])
         fs = factor(E.order())
         assert E.order() % expect == 0
         d1, d2, *_ = E.abelian_group().invariants() + (0,)
@@ -38,63 +100,120 @@ def analyze(primes, x, y, expect=1):
             max2 = max(p for p, _ in fs)
         stats1.append(max1)
         stats2.append(max2)
+        if max1 <= b1 and max2 <= b2:
+            success += 1
         exp2 += E.order().valuation(2)
         exp3 += E.order().valuation(3)
 
     stats1.sort()
     stats2.sort()
     print(f"Generic torsion: ({t1}, {t2})")
+    print(f"Success rate {success/len(primes)*100:.2f}% (B1={b1} B2={b2})")
     l = len(primes)
     exp2, exp3 = float(exp2), float(exp3)
     print(f"avg exponent of 2: {exp2/l:.3f}")
     print(f"avg exponent of 3: {exp3/l:.3f}")
     p25, p50 = stats1[l // 4], stats1[-l // 2]
     p66, p75 = stats1[-l // 3], stats1[-l // 4]
-    print(f"needs B1 {p25=} {p50=} {p66=} {p75=}")
+    print(f"needs B1 {p25=} {p50=} {p66=} {p75=:.3g}")
     p25, p50 = stats2[l // 4], stats2[-l // 2]
     p66, p75 = stats2[-l // 3], stats2[-l // 4]
-    print(f"needs B2 {p25=} {p50=} {p66=} {p75=}")
+    print(f"needs B2 {p25=:.3g} {p50=:.3g} {p66=:.3g} {p75=:.3g}")
 
 
-for size in (32, 48, 64):
-    print(f"=== ===")
-    print(f"=== Working with primes of size {size} ===")
-    print(f"=== ===")
-    p0 = getrandbits(size)
-    ps = random.sample(list(primes(p0, p0 + 1e6)), 2000)
+# For each curve family, we break after the first curve,
+# but test many primes to improve statistics.
+EXTRA_CURVES = False
+
+for size in (24, 32, 48, 64, 80, 96, 112, 128):
+    samples = 8000 if size <= 64 else 2000
+    print(f"################################################")
+    print(f"# Working with {samples} primes of size {size} bits")
+    print(f"################################################")
+    b1 = 1 << (size // 4)
+    b2 = size * b1
+    ps = [random_prime(2 ** (size - 1), 2**size) for _ in range(samples)]
 
     # Basic Edwards curves
-    # Average exp2 = 3 + 2/3
-    # Average exp3 = 2/3
+    # Average exp2 = 3.66
+    # Average exp3 = 0.68
+    # Family X13h, (alpha=-2.28?)
     for y in (3, 4, 5, 6):
-        print(f"=== Trying Edwards curve through (2, {y})")
-        analyze(ps, 2, y, expect=4)
+        print(f"\n=== Trying generic Edwards curve through (2, {y})")
+        analyze(ps, b1, b2, 2, y, expect=4)
+        if not EXTRA_CURVES:
+            break
 
     # Extra 2-torsion (Z/2 x Z/4) when d is a square
     # (3a+5)² + (4a+5)² = 1 + (5a+7)²
-    # Average exp2 = 4 + 1/3
-    # Average exp3 = 2/3
+    # Average exp2 = 4.33
+    # Average exp3 = 0.68
+    # Family X25n (alpha=-2.75?)
     for a in (1, 2, 3):
         x, y = 3 * a + 5, 4 * a + 5
-        print(f"=== Trying Edwards curve Z/2 x Z/4 ({x}, {y})")
-        analyze(ps, x, y, expect=8)
+        print(f"\n=== Trying Edwards curve Z/2 x Z/4 ({x}, {y})")
+        analyze(ps, b1, b2, x, y, expect=8)
+        if not EXTRA_CURVES:
+            break
+
+    # Generic Suyama curve
+    # Average exp2 = 3.33
+    # Average exp3 = 1.69
+    # Family X13 (alpha=-3.15)
+    for sigma in (6, 7, 8):
+        alpha = sigma**2 - 5
+        beta = 4 * sigma
+        a = (beta - alpha) ** 3 * (3 * alpha + beta)
+        v1 = (sigma**2 - 1) * (sigma**2 - 25) * (sigma**4 - 25)
+        x = QQ(alpha * beta) / QQ(2 * v1)
+        y = QQ(alpha**3 - beta**3) / QQ(alpha**3 + beta**3)
+        print(f"\n=== Trying Generic Suyama (σ={sigma})")
+        analyze(ps, b1, b2, x, y, a=a, expect=1)
+        if not EXTRA_CURVES:
+            break
+
+    # Suyama-11 Twisted Edwards (X13f)
+    # https://eecm.cr.yp.to/a1ecm-20100614.pdf (section 5.3)
+    # Average exp2 = 3.66
+    # Average exp3 = 1.69
+    # Family X13f-3B0-3aT2 in Barbulescu et al. (alpha=-3.38)
+    C = EllipticCurve(QQ, [0, -1, 0, -120, 432])
+    PC = C(12, 24)
+    for t in (2, 3, 4, 5):
+        S = t * PC
+        sigma = 24 / S[0] - 1
+        alpha = sigma**2 - 5
+        beta = 4 * sigma
+        r = 48 * S[1] / S[0] ** 2
+        x = (2 * r * sigma) / (sigma - 1) / (sigma + 5) / (sigma**2 + 5)
+        y = (alpha**3 - beta**3) / (alpha**3 + beta**3)
+        print(f"\n=== Trying Suyama-11 Twisted Edwards Z/6 (σ={sigma})")
+        analyze(ps, b1, b2, x, y, a=-1, expect=12)
+        if not EXTRA_CURVES:
+            break
 
     # Good curve Z/12
-    # Average exp2 = 3 + 2/3
-    # Average exp3 = 1 + 2/3
+    # Average exp2 = 3.66
+    # Average exp3 = 1.69
+    # Family X13h-3B0-3aT2 (alpha=-3.38)
     x, y = QQ("5/23"), QQ("-1/7")
     print(f"=== Trying Edwards curve Z/12 ({x}, {y})")
-    analyze(ps, x, y, expect=12)
-    x, y = QQ("8/17"), QQ("20/19")
-    print(f"=== Trying Edwards curve Z/12 ({x}, {y})")
-    analyze(ps, x, y, expect=12)
+    analyze(ps, b1, b2, x, y, expect=12)
+
+    if EXTRA_CURVES:
+        x, y = QQ("8/17"), QQ("20/19")
+        print(f"=== Trying Edwards curve Z/12 ({x}, {y})")
+        analyze(ps, b1, b2, x, y, expect=12)
 
     # Good curve 2x8
-    # Average exp2 = 5 + 1/3
-    # Average exp3 = 2/3
+    # Average exp2 = 5.33
+    # Average exp3 = 0.68
+    # Family X193n (alpha=-3.43)
     x, y = QQ("17/19"), QQ("17/33")
     print(f"=== Trying Edwards curve Z/2 x Z/8 ({x}, {y})")
-    analyze(ps, x, y, expect=16)
-    x, y = QQ("125/91"), QQ("841/791")
-    print(f"=== Trying Edwards curve Z/2 x Z/8 ({x}, {y})")
-    analyze(ps, x, y, expect=16)
+    analyze(ps, b1, b2, x, y, expect=16)
+
+    if EXTRA_CURVES:
+        x, y = QQ("125/91"), QQ("841/791")
+        print(f"=== Trying Edwards curve Z/2 x Z/8 ({x}, {y})")
+        analyze(ps, b1, b2, x, y, expect=16)
