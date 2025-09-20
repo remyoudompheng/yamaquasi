e5e6d4d56293c2d989efbf3474dbacd077c52821
Rémy Oudompheng <remyoudompheng@gmail.com>
2025-09-20 11:38:03 +0200

    Fix typing hints for pymqs

---
+++ pymqs/__init__.py
@@ -0,0 +1,5 @@
+from .pymqs import *
+
+__doc__ = pymqs.__doc__
+if hasattr(pymqs, "__all__"):
+    __all__ = pymqs.__all__
--- pymqs/__init__.pyi
+++ pymqs/__init__.pyi
@@ -3,7 +3,6 @@ def ecm(
     curves: int,
     b1: int,
     b2: float,
-    /,
     verbose: str = "silent",
     threads: int | None = None,
 ) -> list[int]: ...
@@ -11,9 +10,8 @@ def ecm(
 
 def factor(
     n: int,
-    /,
-    algo: str,
-    verbose: str,
+    algo: str = "auto",
+    verbose: str = "silent",
     timeout: float | None = None,
     threads: int | None = None,
     qs_fb_size: int | None = None,
@@ -21,8 +19,10 @@ def factor(
     qs_use_double: bool | None = None,
 ) -> list[int]: ...
 
+
 def factor_smooth(n: int, factor_bits: int) -> list[int]: ...
 
+
 def quadratic_classgroup(
-    d: int, /, verbose: str = "silent", threads: int | None = None
+    d: int, verbose: str = "silent", threads: int | None = None
 ) -> tuple[int, list[int], list[tuple[int, list[int]]]]: ...
